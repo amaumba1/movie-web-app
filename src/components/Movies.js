59ffd7050c6879cf6bc5b58f374134e5fc4382c8
Ally Maumba <amaumba1@gmail.com>
2022-01-01 15:10:48 -0600

    modified the fetch api to get the list of all the movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,46 +4,38 @@ import { Link } from 'react-router-dom';
 class Movies extends Component {
 
   state = {
-    movies: [
-      // {
-      //   id: 1,  title: 'The Shawshank Redemption',    year: 1994,     
-      //   director: 'Frank Darabont',
-      //   poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SX300.jpg',
-      //   rating: 9.3,
-      //   synopsis: 'Two imprisoned'  
-      // },
-      // {
-      //   id: 2,  title: 'The Godfather',    year: 1972,
-      //   director: 'Francis Ford Coppola', 
-      //   poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
-      //   rating: 9.2,
-      //   synopsis: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'
-      // }
-      ]};
+    movies: [],
+    isLoaded: false
+  };
 
     componentDidMount() {
-      fetch('https://ghibliapi.herokuapp.com/films')
+      fetch('http://localhost:4000/v1/movies')
       .then(res => res.json())
-      .then(data => this.setState({ movies: data }));
+      .then(data => this.setState({ movies: data.movies, isLoaded: true }));
     }
 
-  render() {
-      return (
-        <div>
-          <h2>Movie Lists</h2>
-          <ul>
-            {this.state.movies.map(movie => (
-              <li key={movie.id}>
-                <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-                <p>{movie.year}</p>
-                <p>{movie.director}</p>
-                <p>{movie.rating}</p>
-                <p>{movie.synopsis}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
+    render() {
+      const { movies, isLoaded } = this.state;
+      if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <div>
+            <h2>Movie Lists</h2>
+            <ul>
+              {movies.map(movie => (
+                <li key={movie.id}>
+                  <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                  <p>{movie.year}</p>
+                  <p>{movie.director}</p>
+                  <p>{movie.rating}</p>
+                  <p>{movie.synopsis}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+    }
   }
 }
 
